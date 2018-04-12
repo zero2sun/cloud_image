895c9d429265fba74291d6fc511ac9d1824bddb0
zero1sun <zero1sun@gmail.com>
2018-04-12 05:18:43 -0700

    test2

--- hub_centos_centos_07_04_vanilla_01/init.sh
+++ hub_centos_centos_07_04_vanilla_01/init.sh
@@ -4,11 +4,14 @@ echo ==========vagrant destroy==========
 vagrant destroy -f
 
 echo ==========vagrant box copy==========
-cp /root/box_init/hub_centos_centos_07_04_vanilla_01/hub_centos_centos_07_04_vanilla_01.box .
+cp /root/vagrant_image/box_init/hub_centos_centos_07_04_vanilla_01.box .
 echo vagrant box completed
 
 echo ==========vagrant up==========
 vagrant up
+
+echo ==========vagrant provisioning========
+vagrant provision
 
 echo ==========vagrant up==========
 vagrant ssh
